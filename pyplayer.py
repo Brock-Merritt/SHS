4b3b58872bd214b4053c7d7c68557046f62648ac
Your name here <brkmer@gmail.com>
2022-08-10 10:55:09 -0400

    added Videowidget/ box layouts

--- pyplayer.py
+++ pyplayer.py
@@ -1,7 +1,9 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QHBoxLayout, QVBoxLayout
 from PyQt5.QtGui import QIcon, QPalette
 from PyQt5.QtCore import Qt
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
+from PyQt5.QtMultimediaWidgets import QVideoWidget
 
 class Window(QWidget):
     def __init__(self):
@@ -15,8 +17,33 @@ class Window(QWidget):
         p.setColor(QPalette.Window, Qt.blue)
         self.setPalette(p)
 
+        self.create_player()
+
+    def create_player(self):
+        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+
+        videowidget = QVideoWidget()
+
+        self.openBtn = QPushButton('open video')
+
+        hbox = QHBoxLayout()
+        hbox.setContentsMargins(0,0,0,0)
+
+        hbox.addWidget(self.openBtn)
+
+        vbox = QVBoxLayout()
+
+        vbox.addLayout(hbox)
+
+        self.setLayout(vbox)
+
+
+
+
 
 app = QApplication([sys.argv])
 window = Window()
 window.show()
 sys.exit(app.exec_())
+
+
