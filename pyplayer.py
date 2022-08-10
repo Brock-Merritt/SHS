e94e3a46883ee8de4f92a393188c0d3bc6015592
Your name here <brkmer@gmail.com>
2022-08-10 10:28:28 -0400

    added color w qtpalette

--- pyplayer.py
+++ pyplayer.py
@@ -1,14 +1,19 @@
 import sys
 from PyQt5.QtWidgets import QWidget, QApplication
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QPalette
+from PyQt5.QtCore import Qt
 
 class Window(QWidget):
     def __init__(self):
         super().__init__()
 
         self.setWindowIcon(QIcon("player.ico"))
-        self.setWindowTitle("PyPlater")
+        self.setWindowTitle("PyPlayer")
         self.setGeometry(350,100, 700,500)
+
+        p = self.palette()
+        p.setColor(QPalette.Window, Qt.blue)
+        self.setPalette(p)
 
 
 app = QApplication([sys.argv])
